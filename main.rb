1cb59f0f1bc8e3e33ecec994c57633e34e2ad878
Kliment Mamykin <kmamyk@gmail.com>
2015-01-25 00:36:55 -0500

    Spreading data across one day

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ require 'csv'
 require 'time'
 require 'date'
 require 'json'
+require 'active_support/time'
 
 categories = []
 CSV.foreach("./data/Activity_Tracking_db_4_category.csv", headers: true) do |row|
@@ -26,22 +27,25 @@ CSV.foreach("./data/Activity_Tracking_db_4_data.csv", headers: true) do |row|
     observation_date= Date.strptime(row[5], "%m/%d/%y")
     activity = activities.find { |a| a[:code] == row[2] }
     category = categories.find { |c| c[:code] == activity[:category_code] }
+
     data << {
         id: row[0],
         observation_id: row[1],
-        category_code: category[:code],
-        category_label: category[:label],
+        # category_code: category[:code],
+        # category_label: category[:label],
         category_color: category[:color],
-        activity_code: row[2],
-        activity_label: activity[:label],
-        activity_color: activity[:color],
+        # activity_code: row[2],
+        # activity_label: activity[:label],
+        # activity_color: activity[:color],
         actual_start_time: actual_start_time,
         actual_start_time_sec: actual_start_time.to_i,
         actual_end_time: actual_end_time,
         actual_end_time_sec: actual_end_time.to_i,
         duration: (actual_end_time - actual_start_time).to_i,
-        observation_date: observation_date,
-        notes: row[6]
+        start_since_midnight: actual_start_time.seconds_since_midnight.to_i,
+        end_since_midnight: actual_end_time.seconds_since_midnight.to_i
+        # observation_date: observation_date,
+        # notes: row[6]
     }
   rescue StandardError => ex
     puts row
