2bd6d5010c83e85634644570a5eb87cd85b40d19
Kliment Mamykin <kmamyk@gmail.com>
2015-02-02 09:50:30 -0500

    Updated processing code

--- main.rb
+++ main.rb
@@ -23,31 +23,20 @@ end
 data = []
 CSV.foreach("./data/Activity_Tracking_db_4_data.csv", headers: true) do |row|
   begin
-    actual_start_time= Time.strptime(row[3], "%m/%d/%y %H:%M:%S")
-    actual_end_time= Time.strptime(row[4], "%m/%d/%y %H:%M:%S")
-    observation_date= Date.strptime(row[5], "%m/%d/%y")
-    activity = activities.find { |a| a[:code] == row[2] }
-    category = categories.find { |c| c[:code] == activity[:category_code] }
+    actual_start_time= Time.strptime(row[4], "%m/%d/%y %H:%M:%S")
+    actual_end_time= Time.strptime(row[5], "%m/%d/%y %H:%M:%S")
+    # observation_date= Date.strptime(row[8], "%m/%d/%y")
+    # activity = activities.find { |a| a[:code] == row[2] }
+    category = categories.find { |c| c[:label] == row[3] }
 
-    # NOTE: commented keys are not used in the visualization
-    data << {
+    parsed ={
         id: row[0],
         observation_id: row[1],
         category_code: category[:code],
-        # category_label: category[:label],
-        # category_color: category[:color],
-        # activity_code: row[2],
-        # activity_label: activity[:label],
-        # activity_color: activity[:color],
-        # actual_start_time: actual_start_time,
-        # actual_start_time_sec: actual_start_time.to_i,
-        # actual_end_time: actual_end_time,
-        # actual_end_time_sec: actual_end_time.to_i,
         start_since_midnight: actual_start_time.seconds_since_midnight.to_i,
         end_since_midnight: actual_end_time.seconds_since_midnight.to_i
-        # observation_date: observation_date,
-        # notes: row[6]
     }
+    data << parsed
   rescue StandardError => ex
     puts row
     raise ex
